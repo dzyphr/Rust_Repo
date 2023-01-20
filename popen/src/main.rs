2cb2e27f362716e32f7916630c6fd67846dcd3a7
justaguy12345 <ryanberk135@gmail.com>
2023-01-19 18:57:35 -0600

    popen rust first commit

---
+++ popen/src/main.rs
@@ -0,0 +1,27 @@
+use subprocess::{Popen, PopenConfig, Redirection};
+fn main() 
+{
+    //the subprocess crate allows easy access to stdout and stderr
+    let command = "gcc";
+    let argument = "--version";
+    let mut output = String::new();
+    let mut p = Popen::create(&[command, argument], PopenConfig {
+        stdout: Redirection::Pipe, ..Default::default()
+    }).expect("error running {command$} {argument$}");
+    let (out, err) = p.communicate(None).expect("error geting stdout and/or stderr from running {command$} {argument$}");
+    let exitStatus = p.poll();
+    dbg!(&exitStatus);
+    if p.poll() != None
+    {
+        dbg!(format!("{:?}", exitStatus));
+        assert!(format!("{:?}", exitStatus) == ("Some(Exited(0))"), 
+            "Subprocess execution returned unsuccessful exit status! Command being ran was:  ");
+        println!("{} {}", command, argument);
+    } 
+    else 
+    {
+        p.terminate().expect("error terminating subrocess after running {command$} {argument$}");
+    }
+    dbg!(&out);
+    dbg!(&err);
+}
